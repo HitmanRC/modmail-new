9c7d4b8bbc4f58be51417b935a4d4c3fe3b2cbf5
Seth Deegan <jayandseth@gmail.com>
2018-08-02 21:47:48 -0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,23 @@
 const Eris = require('eris');
 const config = require('./config');
+const http = require('http');
+const port = process.env.PORT;
+
+
+// Server keeps the bot up with Uptime Robot pinging it
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('server requested');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`);
+});
 
 const bot = new Eris.CommandClient(config.token, {}, {
   prefix: config.prefix,
